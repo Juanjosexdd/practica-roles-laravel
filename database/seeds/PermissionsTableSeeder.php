74e11b40baa83ac669586372d4d921cf91a1a688
JuanJosexDD7 <JuanJosexDD7>
2019-09-12 12:15:00 -0400

    Sistema de Roles y permisos

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Permission;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	//Users
+        Permission::create([
+        	'name'        => 'Navegar Usuarios',
+        	'slug'        => 'users.index',
+        	'description' => 'Lista y navega todos los usuarios del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Ver detalle de Usuarios',
+        	'slug'        => 'users.show',
+        	'description' => 'Ver en detalle cada usuario del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Edición de Usuarios',
+        	'slug'        => 'users.edit',
+        	'description' => 'Editar cualquier dato de un usuario del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Eliminar Usuarios',
+        	'slug'        => 'users.destroy',
+        	'description' => 'Eliminar cualquier usuario del sistema',
+        ]);
+        //Roles
+        Permission::create([
+        	'name'        => 'Navegar roles',
+        	'slug'        => 'roles.index',
+        	'description' => 'Lista y navega todos los roles del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Ver detalle de roles',
+        	'slug'        => 'roles.show',
+        	'description' => 'Ver en detalle cada rol del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Creación de roles',
+        	'slug'        => 'roles.create',
+        	'description' => 'Editar cualquier dato de un rol del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Edición de roles',
+        	'slug'        => 'roles.edit',
+        	'description' => 'Editar cualquier dato de un rol del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Eliminar roles',
+        	'slug'        => 'roles.destroy',
+        	'description' => 'Eliminar cualquier rol del sistema',
+        ]);
+        //Products
+        Permission::create([
+        	'name'        => 'Navegar productos',
+        	'slug'        => 'products.index',
+        	'description' => 'Lista y navega todos los producto del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Ver detalle de productos',
+        	'slug'        => 'products.show',
+        	'description' => 'Ver en detalle cada producto del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Creación de productos',
+        	'slug'        => 'products.create',
+        	'description' => 'Editar cualquier dato de un producto del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Edición de productos',
+        	'slug'        => 'products.edit',
+        	'description' => 'Editar cualquier dato de un producto del sistema',
+        ]);
+        Permission::create([
+        	'name'        => 'Eliminar productos',
+        	'slug'        => 'products.destroy',
+        	'description' => 'Eliminar cualquier producto del sistema',
+        ]);
+    }
+}
